a25748c74e6056b533648380a185304747a1212e
Sebastian Zdanowicz <14982968+szdanowi@users.noreply.github.com>
2020-07-28 12:25:41 +0200

    I can do Picasso too! :D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate cairo;
+extern crate rand;
+
+use cairo::{ ImageSurface, Format, Context };
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let max_x = 1024;
+    let max_y = 768;
+
+    let surface = ImageSurface::create(Format::ARgb32, max_x, max_y).expect("Failed to create a surface");
+    let context = Context::new(&surface);
+
+    context.set_source_rgb(1.0, 1.0, 1.0);
+    context.paint();
+
+    context.set_source_rgb(0.1, 0.1, 0.1);
+    for _line in 0..10 {
+        let x = rand::random::<f64>() * f64::from(max_x);
+        let y = rand::random::<f64>() * f64::from(max_y);
+        context.line_to(x, y);
+    }
+    context.stroke();
+
+    let mut file = File::create("result.png").expect("Failed to create a file");
+    surface.write_to_png(&mut file).expect("Failed to write to the file");
 }
